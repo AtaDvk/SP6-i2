ac9255911555e736b3416f26b2aaf32d9f940fc8
bot <127.0.0.1>
2015-02-14 22:36:47 +0100

    Blah

--- src/Bleach/Loader/Discette.java
+++ src/Bleach/Loader/Discette.java
@@ -60,10 +60,8 @@ public class Discette {
 	try {
 	    entries = json.fromJson(readFile(pathToJSON), JsonObject[].class);
 	} catch (JsonSyntaxException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	} catch (IOException e) {
-	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
 
